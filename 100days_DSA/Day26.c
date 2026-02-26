2f73180e886578c5469a9f040450a5aeab44f38d
ajain347 <atisheyj35@gmail.com>
2026-02-26 17:45:03 +0530

    codes

---
+++ 100days_DSA/Day26.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Node 
+{
+    struct Node* next;
+    struct Node* prev;
+} Node;
+
+int main() 
+{
+    int n;
+    scanf("%d", &n);
+
+    if (n <= 0) return 0;
+
+    Node *head = NULL, *tail = NULL;
+
+    for (int i = 0; i < n; i++) 
+    {
+        int value;
+        scanf("%d", &value);
+
+        Node* newNode = (Node*)malloc(sizeof(Node));
+        newNode->data = value;
+        newNode->next = NULL;
+        newNode->prev = NULL;
+
+        if (head == NULL) 
+        {
+            head = newNode;
+            tail = newNode;
+        } 
+        else 
+        {
+            tail->next = newNode;
+            newNode->prev = tail;
+            tail = newNode;
+        }
+    }
+
+    Node* temp = head;
+    while (temp != NULL) 
+    {
+        printf("%d ", temp->data);
+        temp = temp->next;
+    }
+
+    return 0;
+}
